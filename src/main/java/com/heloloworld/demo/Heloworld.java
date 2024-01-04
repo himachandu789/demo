f827edcb5c3a7ed8bfe5e7902920369329281e32
himachandu789 <himachandu789@gmail.com>
2024-01-04 14:18:51 +0530

    commit

--- src/main/java/com/heloloworld/demo/Heloworld.java
+++ src/main/java/com/heloloworld/demo/Heloworld.java
@@ -1,30 +1,27 @@
 package com.heloloworld.demo;
 
-import ch.qos.logback.core.model.Model;
 import com.heloloworld.demo.model.Message;
-
 import com.heloloworld.demo.repo.MessageRepo;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-
-@RestController
+@Controller
 public class Heloworld {
 
     @Autowired
-    private MessageRepo messageRepo;
+    MessageRepo messageRepo;
 
     @GetMapping("/hello")
-    public ResponseEntity<String> helloWorld() {
+    public ModelAndView helloWorld() {
         String messageContent = "Hello, World! Helloooooo";
         Message message = new Message();
         message.setContent(messageContent);
         messageRepo.save(message);
 
-        return ResponseEntity.ok(messageContent);
+        ModelAndView modelAndView = new ModelAndView("helloworld");
+        modelAndView.addObject("message", messageContent);
+        return modelAndView;
     }
 }
